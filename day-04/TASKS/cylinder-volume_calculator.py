ca81c2f59d91c128650ac35047a911db347025ae
BryceMadelo <brycemadelo07@gmail.com>
2024-02-21 13:03:57 +0800

    Day 4 Completed.

--- day-04/TASKS/cylinder-volume_calculator.py
+++ day-04/TASKS/cylinder-volume_calculator.py
@@ -1,25 +1,26 @@
 import math
 
-# Write the code ↓ to read the radius and height of a cylinder from the user.
-# Be cautious when reading input of various data types.
+print("CYLINDER VOLUME CALCULATOR FOR ALF")
+print("")
 
+#Input Values
+while True:
+    try:
+        r = float(input("Please, enter the radius of the cylinder: "))
+        h = float(input("Please, enter the height of the cylinder: "))
+        
+        if r <= 0 or h <= 0:
+            print("")
+            print("Please, enter valid values of radius and height.")
+            print("")
+        else:
+            break   
+    except ValueError:
+        print("")
+        print("Invalid input. Please enter a valid number.")
+        print("")
 
-
-
-
-
-# Write the code ↓ to calculate the volume of the cylinder using the formula V = πr^2h.
-# Formula to calculate the volume (V) of a cylinder:
-# V = π * r^2 * h
-
-
-
-
-
-# Write the code ↓ to display the calculated volume with 2 decimal places.
-# Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
+#Output
+volume = math.pi * r ** 2 * h
+print("")
+print(f"The hypotenuse of the right-angled triangle is: {volume:.2f}")
--- day-04/TASKS/hypotenuse_calculator.py
+++ day-04/TASKS/hypotenuse_calculator.py
@@ -1,26 +1,18 @@
 import math
 
-# Write the code ↓ to read the lengths of the two shorter sides of a right-angled triangle from the user.
-# Be cautious when reading input of various data types.
+print("HYPOTENUSE CALCULATOR FOR ALF")
+print("")
 
+#Input Values
+while True:
+    try:
+        sideA = float(input("Please, enter the length of side A: "))
+        sideB = float(input("Please, enter the length of side B: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
 
-
-
-
-
-# Write the code ↓ to calculate the hypotenuse using the Pythagorean theorem.
-# The Pythagorean theorem: c^2 = a^2 + b^2, where c is the hypotenuse.
-
-
-
-
-
-
-# Write the code ↓ to display the calculated hypotenuse.
-# Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
-
+#Output
+hypotenuse = math.sqrt(sideA ** 2 + sideB ** 2)
+print("")
+print(f"The hypotenuse of the right-angled triangle is: {hypotenuse:.2f}")
