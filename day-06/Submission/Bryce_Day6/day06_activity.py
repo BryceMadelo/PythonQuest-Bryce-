f4c76db61c88cac2656427f3b4c43a60cf81ee1c
BryceMadelo <brycemadelo07@gmail.com>
2024-02-21 20:16:58 +0800

    Day 6 Completed.

---
+++ day-06/Submission/Bryce_Day6/day06_activity.py
@@ -0,0 +1,7 @@
+try:
+    with open("self-introduction.txt", "r") as file:
+        print(file.read())
+except FileNotFoundError:
+    print("The file 'self-introduction.txt' can't be found.")
+except Exception as e:
+    print(f"An error occured: {e}")
